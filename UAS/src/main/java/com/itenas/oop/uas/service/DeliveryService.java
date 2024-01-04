f84fd2df3d8e9bd989aa75693de1a8a18b23b3d1
fatimaul <fatimah.aulia@mhs.itenas.ac.id>
2024-01-04 15:18:08 +0700

    service

---
+++ UAS/src/main/java/com/itenas/oop/uas/service/DeliveryService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.itenas.oop.uas.service;
+
+import com.itenas.oop.uas.repository.CrudRepository;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface DeliveryService extends CrudRepository<Object, Object>{
+    public int getDeliveryCount();
+}
---
+++ UAS/src/main/java/com/itenas/oop/uas/service/TransaksiService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.itenas.oop.uas.service;
+
+import com.itenas.oop.uas.pojo.OrderQuantity;
+import com.itenas.oop.uas.pojo.Transaksi;
+import com.itenas.oop.uas.repository.CrudRepository;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface TransaksiService extends CrudRepository<Object, Object>{
+    public int hargaCucian(OrderQuantity orderQuantity, int harga);
+    public int totalHarga(Transaksi transaksi);
+    public int getTransaksiCount();
+}
